4148dca69a72065482f77dcf0194a8195d059b5b
soymizra <jorgemizrain@gmail.com>
2020-11-07 15:54:35 -0600

    Integracion de bootstrap al proyecto

--- 01_desarrolloweb_python/proyecto/app/__init__.py
+++ 01_desarrolloweb_python/proyecto/app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap()
 
 # importamos la rutas desde views
 from .views import page
 
 def create_app(config):
+    # configuracion del servidor desde un objeto
     app.config.from_object(config)
+
+    #inicializar bootstrap
+    bootstrap.init_app(app)
+
     # configuramos las rutas de views
     app.register_blueprint(page)
     return app
