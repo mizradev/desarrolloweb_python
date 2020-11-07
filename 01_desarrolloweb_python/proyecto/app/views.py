5f68ccbe5c2ff6acb08e36bb3ad3744b527ed704
soymizra <jorgemizrain@gmail.com>
2020-11-07 15:37:40 -0600

    Archivos estaticos y Manejo de errores 404

--- 01_desarrolloweb_python/proyecto/app/views.py
+++ 01_desarrolloweb_python/proyecto/app/views.py
@@ -3,6 +3,12 @@ from flask import render_template
 
 page = Blueprint('page', __name__)
 
+#Error 404
+@page.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+
 @page.route('/')
 def index():
     return render_template('index.html')
