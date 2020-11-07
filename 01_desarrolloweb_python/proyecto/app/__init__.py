336f699285c1d9911e85fec33012ae513da713ff
soymizra <jorgemizrain@gmail.com>
2020-11-07 15:45:25 -0600

    Configuraciones de desarrollo entorno de desarrollo

--- 01_desarrolloweb_python/proyecto/app/__init__.py
+++ 01_desarrolloweb_python/proyecto/app/__init__.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 # importamos la rutas desde views
 from .views import page
 
-def create_app():
+def create_app(config):
+    app.config.from_object(config)
     # configuramos las rutas de views
     app.register_blueprint(page)
     return app
