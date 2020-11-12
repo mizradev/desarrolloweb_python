05c6a1aadbee2642a7e31eb3060746cb5b215648
soymizra <jorgemizrain@gmail.com>
2020-11-12 15:19:43 -0600

    Renderizar templates html

--- 02_curso_apps_web_django/introduccion/facilito_store/facilito_store/views.py
+++ 02_curso_apps_web_django/introduccion/facilito_store/facilito_store/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('Hola desde views.py')
+	#return HttpResponse('Hola desde views.py')
+	return render(request,'index.html',{
+		#context
+	})
