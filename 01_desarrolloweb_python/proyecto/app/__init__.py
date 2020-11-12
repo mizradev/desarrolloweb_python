dc63e4b07de7bb9c459d463339f16533bb82e9f6
soymizra <jorgemizrain@gmail.com>
2020-11-12 14:35:27 -0600

    Formularios

--- 01_desarrolloweb_python/proyecto/app/__init__.py
+++ 01_desarrolloweb_python/proyecto/app/__init__.py
@@ -3,6 +3,10 @@ from flask_bootstrap import Bootstrap
 # CSRF Protect
 from flask_wtf.csrf import CSRFProtect
 
+# se importa SQLAlchemy ORM
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 app = Flask(__name__)
 bootstrap = Bootstrap()
 csrf = CSRFProtect() # csrf protect
@@ -10,9 +14,15 @@ csrf = CSRFProtect() # csrf protect
 # importamos la rutas desde views
 from .views import page
 
+# Importar el modelo User
+from .models import User
+
 def create_app(config):
     # configuracion del servidor desde un objeto
     app.config.from_object(config)
+
+    # inicializamos el ORM SQLAlchemy
+    db.init_app(app)
 
     # inicializamos la proteccion contra CSRF
     csrf.init_app(app)
@@ -22,4 +32,10 @@ def create_app(config):
 
     # configuramos las rutas de views
     app.register_blueprint(page)
+
+    # se crean las tablas en la base de datos
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
     return app
--- 01_desarrolloweb_python/proyecto/config.py
+++ 01_desarrolloweb_python/proyecto/config.py
@@ -3,6 +3,9 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    #SQLAlchemy_DATABASE_URI = 'GESTOR://USUARIO:CONTRASEÑA@DIRECCION/NOMBRE_BASE_DE_DATO'
+    SQLALCHEMY_DATABASE_URI = 'mysql://userdb:cmfFPWvBNDrjwgyV@127.0.0.1:8888/task_project_python'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 config = {
